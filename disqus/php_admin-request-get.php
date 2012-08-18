febcad1e08b926c8be3303a99ecb50bec50df47c
Ps0ke <ps0ke.mail@gmail.com>
2012-08-18 19:47:55 +0200

    Removed unnecessary code from the disqus plugin.

--- disqus/php_admin-request-get.php
+++ disqus/php_admin-request-get.php
@@ -1,7 +1,5 @@
 <?php
-if(preg_match("/admin/", getcwd()) && isset($_GET['disqus-shortname'])){
-	if(file_exists($filename))
-		unlink($filename);
+if(preg_match("/admin/", getcwd()) && isset($_GET['disqus-shortname']))
 	file_put_contents(BASE_PATH."plugins/disqus/html_head.php", "<?php\ndefine('DISQUS_SHORTNAME', \"".$_GET['disqus-shortname']."\");\n?>");
-}
+
 ?>
